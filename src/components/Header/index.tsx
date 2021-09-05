0bace802355ea46acbe6f7b3fcc4893482fac26b
Mohamed Ismail <ismailreality1@gmail.com>
2021-09-05 18:36:28 +0200

    fix(UI): header title and filter section

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,9 @@ import './index.css';
 export default function Header() {
   return (
     <header className="app-header">
-      <h1 className="app-title">One Stop Beverages</h1>
+      <div className="app-title-section">
+        <h1 className="app-title">One Stop Beverages</h1>
+      </div>
       <FiltersBar />
     </header>
   );
