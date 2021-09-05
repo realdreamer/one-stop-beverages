de2e341d66996903b01f9ddcd336a8a4fb76815e
Mohamed Ismail <ismailreality1@gmail.com>
2021-09-05 16:44:30 +0200

    chore: create new header separate component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import './index.css';
+
+export default function Header() {
+  return (
+    <header className="app-header">
+      <h1 className="app-title">One Stop Beverages</h1>
+    </header>
+  );
+}
